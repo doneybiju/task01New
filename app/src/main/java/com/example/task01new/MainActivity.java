48fb0b94e29a0fca7cfb14076dd895e24e51a9f2
doneybiju <doney.john@knf.stud.vu.lt>
2023-11-17 17:20:31 +0200

    3rd Commit with Color Change

--- app/src/main/java/com/example/task01new/MainActivity.java
+++ app/src/main/java/com/example/task01new/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.task01new;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private TextView textView;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2); // Add this line to find the second button
         textView = findViewById(R.id.textView2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle button click
-                textView.setText("Text Changed :)");
+                textView.setText("Button Clicked!");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView.setTextColor(Color.BLUE);
             }
         });
     }
