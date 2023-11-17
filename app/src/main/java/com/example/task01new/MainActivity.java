ddbcae408f34b927efe98913bc18d2d577aca092
doneybiju <doney.john@knf.stud.vu.lt>
2023-11-17 17:06:57 +0200

    2nd Commit

--- app/src/main/java/com/example/task01new/MainActivity.java
+++ app/src/main/java/com/example/task01new/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.task01new;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+                textView.setText("Text Changed :)");
+            }
+        });
     }
 }
